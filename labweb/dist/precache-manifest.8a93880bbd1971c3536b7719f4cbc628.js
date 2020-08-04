eeae056902b85143ec577c2c98a1a22c27b704a8
Stanislav Bohm <spirali@kreatrix.org>
2020-08-04 16:20:20 +0200

    libweb build

---
+++ labweb/dist/precache-manifest.8a93880bbd1971c3536b7719f4cbc628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea6670c58c0aa49ec916af38f90981f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8799d78babdf7c255720",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a1216172caed16d26526",
+    "url": "/static/js/2.f6540501.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/static/js/2.f6540501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8799d78babdf7c255720",
+    "url": "/static/js/main.9593f518.chunk.js"
+  },
+  {
+    "revision": "0935c01549041d1d6942",
+    "url": "/static/js/runtime-main.cdbae762.js"
+  }
+]);
